a16f35a77e110e02396aac59522421897b685fb7
Aram Avila-Herrera <Aram.Avila-Herrera@ucsf.edu>
2014-10-22 19:00:30 -0700

    Keeps tab as data.table; loads empty *.tab
    Adds roxygen comments
    Note: Missing files give same warning as an empty *.tab

--- coevo/R/tabIO.R
+++ coevo/R/tabIO.R
@@ -1,16 +1,30 @@
-#R
-# Reads an already formatted *.tab file
-#   - tab separated
-#   - first row is header
-#   - first two columns are alignment column indices
-#   - column names do not start with '[r|z]_'
+#' @family tab loading functions
+#' @seealso \code{\link{loadTab}}
 
-#requires(data.table)
+#' Reads a *.tab formatted file and returns a data.table.
+#'
+#' @param fn A filename to read.
+#' @return A data.table or NULL.
+#' @examples
+#' loadTab('results.tab')
+#' list_of_tables = lapply(list_of_filenames, loadTab)
 
+#' @section *.tab file format:
+#' *.tab files are:
+#' \itemize{
+#'     \item tab separated
+#'     \item first row is header
+#'     \item first two columns are alignment column indices
+#'     \item column names do not start with '[r|z]_'
+#' }
 loadTab = function(fn){
-	# generic load tab
-    tab = data.table::fread(fn, header = TRUE, sep = '\t')
-    tab = data.frame(tab)
+    tab = try(
+        data.table::fread(fn, header = TRUE, sep = '\t')
+    )
+    if(class(tab) == 'try-error'){
+        warning(paste('loadTab: Read no lines from', fn))
+        return(NULL)
+    }
 	return(tab)
 }
 
